c47648ce3796a36b8e09aa77c8c49824bf8b62b6
LuKane <1169604556@qq.com>
2016-06-20 10:41:03 +0800

    1.hash
    hash

---
+++ KNBannerView/KNBannerView/NSString+hash.m
@@ -0,0 +1,34 @@
+//
+//  NSString+hash.h
+//  KNBannerView
+//
+//  Created by LuKane on 14/12/15.
+//  Copyright © 2015年 KNKane. All rights reserved.
+//
+
+#import "NSString+Hash.h"
+#import <CommonCrypto/CommonCrypto.h>
+
+@implementation NSString (Hash)
+
+#pragma mark - 散列函数
+- (NSString *)md5String {
+    const char *str = self.UTF8String;
+    unsigned char buffer[CC_MD5_DIGEST_LENGTH];
+    
+    CC_MD5(str, (CC_LONG)strlen(str), buffer);
+    
+    return [self stringFromBytes:buffer length:CC_MD5_DIGEST_LENGTH];
+}
+
+- (NSString *)stringFromBytes:(unsigned char *)bytes length:(int)length {
+    NSMutableString *strM = [NSMutableString string];
+    
+    for (int i = 0; i < length; i++) {
+        [strM appendFormat:@"%02x", bytes[i]];
+    }
+    
+    return [strM copy];
+}
+
+@end
